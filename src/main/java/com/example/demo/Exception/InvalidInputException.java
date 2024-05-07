81510a04ac9e1309c1d86637dfa9479ced52e199
Maliek Borwin <Maliek.Borwin@autotrader.co.uk>
2024-05-07 09:18:13 +0100

    custom error handler

---
+++ src/main/java/com/example/demo/Exception/InvalidInputException.java
@@ -0,0 +1,20 @@
+package com.example.demo.Exception;
+
+public class InvalidInputException extends Exception {
+
+    public InvalidInputException() {
+        super();
+    }
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+
+    public InvalidInputException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public InvalidInputException(Throwable cause) {
+        super(cause);
+    }
+}
